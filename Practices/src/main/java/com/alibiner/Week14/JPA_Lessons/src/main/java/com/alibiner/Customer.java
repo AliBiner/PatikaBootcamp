d74632cea12d72868fbd29adbd0db9ca7861546d
Ali Biner <alibiner.github@gmail.com>
2025-09-30 18:20:04 +0300

    update lombok annotations

--- Practices/src/main/java/com/alibiner/Week14/JPA_Lessons/src/main/java/com/alibiner/Customer.java
+++ Practices/src/main/java/com/alibiner/Week14/JPA_Lessons/src/main/java/com/alibiner/Customer.java
@@ -1,11 +1,19 @@
 package com.alibiner;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Customer {
 
     @Id
@@ -27,46 +35,14 @@ public class Customer {
     private Gender gender;
 
 
-    public Customer() {
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDateTime getOnDate() {
-        return onDate;
-    }
-
-    public void setOnDate(LocalDateTime onDate) {
-        this.onDate = onDate;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", onDate=" + onDate +
+                ", gender=" + gender +
+                '}';
     }
 }
