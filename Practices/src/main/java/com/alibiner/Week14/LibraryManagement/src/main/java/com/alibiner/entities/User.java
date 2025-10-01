622f604054ae8cc6eae55da5ff7596d70001ec93
Ali Biner <alibiner.github@gmail.com>
2025-10-01 10:32:29 +0300

    initial user entity

---
+++ Practices/src/main/java/com/alibiner/Week14/LibraryManagement/src/main/java/com/alibiner/entities/User.java
@@ -0,0 +1,33 @@
+package com.alibiner.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long id;
+
+    @Column(name = "user_name",nullable = false,length = 50)
+    private String name;
+
+    @Column(name = "user_birth_date", nullable = false)
+    private LocalDate birthDate;
+
+
+    //Todo - Relation to BookBorrowing
+
+}
