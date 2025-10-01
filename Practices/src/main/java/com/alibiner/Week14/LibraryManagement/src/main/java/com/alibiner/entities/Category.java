10e914f3d6c69efd26ed83072dfce8ff7d42c064
Ali Biner <alibiner.github@gmail.com>
2025-10-01 10:45:51 +0300

    initial Category entity

---
+++ Practices/src/main/java/com/alibiner/Week14/LibraryManagement/src/main/java/com/alibiner/entities/Category.java
@@ -0,0 +1,27 @@
+package com.alibiner.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private long id;
+
+    @Column(name = "category_name", nullable = false, unique = true , length = 50)
+    private String name;
+
+    @Column(name = "category_description")
+    private String description;
+}
