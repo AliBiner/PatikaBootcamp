3ab0a16df7992c4aa128cdbc01d5d9cfc07654f4
Ali Biner <alibiner.github@gmail.com>
2025-10-01 10:50:13 +0300

    initial Author entity

---
+++ Practices/src/main/java/com/alibiner/Week14/LibraryManagement/src/main/java/com/alibiner/entities/Author.java
@@ -0,0 +1,34 @@
+package com.alibiner.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private long id;
+
+    @Column(name = "author_name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "author_birth_date", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(name = "author_contry", length = 50, nullable = false)
+    private String country;
+
+    //Todo - Relate Book
+}
