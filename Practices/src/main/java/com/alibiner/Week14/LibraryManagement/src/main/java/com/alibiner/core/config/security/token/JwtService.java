b382b6a1ca22816bb8fd4c6bf65066f6a950d635
Ali Biner <alibiner.github@gmail.com>
2025-10-09 15:02:59 +0300

    config jwt access-token

---
+++ Practices/src/main/java/com/alibiner/Week14/LibraryManagement/src/main/java/com/alibiner/core/config/security/token/JwtService.java
@@ -0,0 +1,100 @@
+package com.alibiner.core.config.security.token;
+
+import java.util.*;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.MacAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.function.*;
+
+@Service
+public class JwtService {
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long jwtExpiration;
+
+    private final MacAlgorithm alg = Jwts.SIG.HS256;
+    private SecretKey key;
+
+    @PostConstruct
+    public void init(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys .hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    public long getExpirationTime() {
+        return jwtExpiration;
+    }
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) {
+        return Jwts
+                .builder()
+                //.header().keyId(userDetails.getUsername()).and()
+                .subject(userDetails.getUsername())
+                .claims(extraClaims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key, alg)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        JwtParserBuilder parser = Jwts.parser();
+        JwtParser build = parser.verifyWith(key).build();
+        Jwt<?, ?> parse = build.parseSignedClaims(token);
+        Object payload = parse.getPayload();
+        return (Claims) payload;
+    }
+
+//    Jwts
+//            .parser()
+//            .verifyWith(key)
+//                .build()
+//                .parse(token.subSequence(0,token.length()))
+//            .getPayload();
+
+}
